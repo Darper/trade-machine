c63bcb8cd68e5ba2d37f00796a1324c0311e4930
dharper <dharper@digitalriver.com>
2017-11-14 07:46:50 -0600

    Ejected create-react-app and updated app.js to have team rosters and pull stats properly

---
+++ src/App.js
@@ -0,0 +1,229 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import rosters from './data/rosters.js';
+import { Dropdown, Modal, Button } from 'react-atlas';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import '../node_modules/react-atlas/lib/atlasThemes.min.css';
+import NBA from 'nba';
+import nbajs from 'nba.js';
+var request = require('request');
+
+request({
+  uri: 'https://www.parsehub.com/api/v2/projects/tN2CgU3QsD-t/last_ready_run/data',
+  method: 'GET',
+  gzip: true,
+  qs: {
+    api_key: "tTyawwH1NEZr",
+    format: "json"
+  }
+}, function(err, resp, body) {
+  console.log(body);
+});
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    let teams = [];
+    let parsedRosters = [];
+    for (var property in rosters) {
+      if (rosters.hasOwnProperty(property)) {
+          teams.push(property)
+          let team = [];
+          rosters[property].forEach(function(player, pIndex){
+            let playerObject = {};
+            const parsedName = player.name.slice(0, player.name.indexOf(","));
+            playerObject.name = parsedName.replace(/[^\w\s]/gi, '')
+            team.push(playerObject);
+          });
+          parsedRosters[property] = team;
+      }
+    }
+
+    this.state = {
+      teams: teams,
+      rosters: parsedRosters,
+      players: [],
+      firstTeam: null,
+      firstTeamStats: {},
+      secondTeam: null,
+      secondTeamStats: {},
+      statsActive: false,
+      statsActivePlayer: {}
+    };
+  }
+
+  componentDidMount() {
+    NBA.stats
+      .playerStats()
+      .then(response =>
+        this.setState({ players: response.leagueDashPlayerStats }, function(){
+          console.log(this.state.players);
+        })
+      );
+  }
+
+  setTeam = (value, first) => {
+    if (first) {
+      this.setState({ firstTeam: value });
+    } else {
+      this.setState({ secondTeam: value });
+    }
+  };
+
+  getRoster = teamName => {
+    const team = this.state.rosters[teamName];
+    return team;
+  };
+
+  showPlayerStats = playerName => {
+    const player = this.state.players.find(x => x.playerName === playerName);
+    console.log(player);
+    const playerArray = [];
+    playerArray.push(player);
+    this.setState({ statsActivePlayer: playerArray, statsActive: true });
+  };
+
+  handleToggle = () => {
+    this.setState({ statsActive: false });
+  };
+
+  render() {
+    const teams = this.state.teams.map((team, index) => (
+      <li value={team} key="{index}">
+        {team}
+      </li>
+    ));
+
+    function updateTeamStats(player, selected, team) {
+
+    }
+
+    const options = {
+      onRowClick: (row, columnIndex, rowIndex) =>
+        this.showPlayerStats(row.name),
+      onRowDoubleClick: function(row) {
+        alert(`You double click row id: ${row.name}`);
+      }
+    };
+
+    function onRowSelect(row, isSelected, e) {
+      console.log(e);
+      let rowStr = '';
+      for (const prop in row) {
+        rowStr += prop + ': "' + row[prop] + '"';
+      }
+      console.log(e);
+      alert(`is selected: ${isSelected}, ${rowStr}`);
+    }
+
+    const selectRowProp = {
+      mode: 'checkbox',
+      onSelect: onRowSelect
+    };
+
+    return (
+      <div
+        className="App"
+        style={{ margin: 'auto', width: '100%', textAlign: 'center' }}
+      >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        {(this.state.firstTeam !== null && this.state.secondTeam !== null) && (
+          <Button primary style={{display: 'block', margin: '10px auto'}}>Analyze Trade</Button>
+        )}
+        <div
+          style={{
+            width: '30%',
+            margin: '0 auto',
+            display: 'inline-block',
+            verticalAlign: 'top'
+          }}
+        >
+          <Dropdown
+            defaultText="Select One ..."
+            customLabel="Select First Team"
+            onChange={value => {
+              this.setTeam(value, true);
+            }}
+          >
+            {teams}
+          </Dropdown>
+          <br />
+          {this.state.firstTeam !== null && (
+            <BootstrapTable
+              data={this.getRoster(this.state.firstTeam)}
+              options={options}
+              selectRow={selectRowProp}
+            >
+              <TableHeaderColumn dataField="name" isKey={true}>
+                Name
+              </TableHeaderColumn>
+            </BootstrapTable>
+          )}
+        </div>
+        <div
+          style={{
+            width: '30%',
+            margin: '0 auto',
+            display: 'inline-block',
+            verticalAlign: 'top'
+          }}
+        >
+          <Dropdown
+            defaultText="Select One ..."
+            customLabel="Select Second Team"
+            onChange={value => {
+              this.setTeam(value, false);
+            }}
+          >
+            {teams}
+          </Dropdown>
+          <br />
+          {this.state.secondTeam !== null && (
+            <BootstrapTable
+              data={this.getRoster(this.state.secondTeam)}
+              options={options}
+              selectRow={selectRowProp}
+            >
+              <TableHeaderColumn dataField="name" isKey={true}>
+                Name
+              </TableHeaderColumn>
+            </BootstrapTable>
+          )}
+        </div>
+        <Modal
+          overlay
+          active={this.state.statsActive}
+          onOverlayClick={this.handleToggle}
+          onEscKeyDown={this.handleToggle}
+          title={this.state.statsActivePlayer.playerName}
+        >
+          <BootstrapTable data={this.state.statsActivePlayer}>
+            <TableHeaderColumn dataField="gp" isKey={true}>
+              Games
+            </TableHeaderColumn>
+            <TableHeaderColumn dataField="fgm">FGM</TableHeaderColumn>
+            <TableHeaderColumn dataField="fga">FGA</TableHeaderColumn>
+            <TableHeaderColumn dataField="fgPct">FG%</TableHeaderColumn>
+            <TableHeaderColumn dataField="ftm">FTM</TableHeaderColumn>
+            <TableHeaderColumn dataField="fta">FTA</TableHeaderColumn>
+            <TableHeaderColumn dataField="ftPct">FT%</TableHeaderColumn>
+            <TableHeaderColumn dataField="fG3M">3PM</TableHeaderColumn>
+            <TableHeaderColumn dataField="reb">REB</TableHeaderColumn>
+            <TableHeaderColumn dataField="ast">AST</TableHeaderColumn>
+            <TableHeaderColumn dataField="stl">STL</TableHeaderColumn>
+            <TableHeaderColumn dataField="blk">BLK</TableHeaderColumn>
+            <TableHeaderColumn dataField="pts">PTS</TableHeaderColumn>
+          </BootstrapTable>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/data/rosters.js
@@ -0,0 +1,490 @@
+const rosters = {
+ "Derek Harper": [
+  {
+   "name": "Mike Conley, Mem PG"
+  },
+  {
+   "name": "Gary Harris, Den SG DTD"
+  },
+  {
+   "name": "Giannis Antetokounmpo, Mil SF, PF"
+  },
+  {
+   "name": "Joe Ingles, Utah PF, SF"
+  },
+  {
+   "name": "Karl-Anthony Towns, Min C"
+  },
+  {
+   "name": "Jamal Murray, Den SG, PG"
+  },
+  {
+   "name": "Joel Embiid, Phi C"
+  },
+  {
+   "name": "Hassan Whiteside, Mia PF, C"
+  },
+  {
+   "name": "Patrick Beverley, LAC PG DTD"
+  },
+  {
+   "name": "James Harden, Hou SG, PG"
+  },
+  {
+   "name": "Carmelo Anthony, OKC SF, PF DTD"
+  },
+  {
+   "name": "Bobby Portis, Chi PF"
+  }
+ ],
+ "Kent Johnson": [
+  {
+   "name": "Lou Williams, LAC PG, SG"
+  },
+  {
+   "name": "Avery Bradley, Det SG"
+  },
+  {
+   "name": "Kelly Oubre Jr., Wsh SF"
+  },
+  {
+   "name": "Kevin Durant, GS SF, PF"
+  },
+  {
+   "name": "Nikola Vucevic, Orl C"
+  },
+  {
+   "name": "Jordan Clarkson, LAL PG, SG"
+  },
+  {
+   "name": "Markieff Morris, Wsh PF"
+  },
+  {
+   "name": "Al Horford, Bos C"
+  },
+  {
+   "name": "Dirk Nowitzki, Dal PF"
+  },
+  {
+   "name": "Marcus Morris, Bos PF"
+  },
+  {
+   "name": "Dennis Smith Jr., Dal PG"
+  },
+  {
+   "name": "Domantas Sabonis, Ind PF, C"
+  },
+  {
+   "name": "Gordon Hayward*, Bos SG, SF O"
+  }
+ ],
+ "JD Mehlhorn": [
+  {
+   "name": "Tyler Johnson, Mia SG, PG"
+  },
+  {
+   "name": "Malik Monk, Cha SG"
+  },
+  {
+   "name": "Harrison Barnes, Dal SF"
+  },
+  {
+   "name": "LaMarcus Aldridge, SA PF"
+  },
+  {
+   "name": "Draymond Green, GS SF, PF, C"
+  },
+  {
+   "name": "Reggie Jackson, Det PG"
+  },
+  {
+   "name": "Serge Ibaka, Tor PF"
+  },
+  {
+   "name": "Jimmy Butler, Min SG, SF"
+  },
+  {
+   "name": "Eric Bledsoe, Mil PG"
+  },
+  {
+   "name": "Brook Lopez, LAL C"
+  },
+  {
+   "name": "Dwight Howard, Cha C"
+  },
+  {
+   "name": "Justin Jackson, Sac SF"
+  },
+  {
+   "name": "Nicolas Batum*, Cha SF, SG O"
+  }
+ ],
+ "Ian Western": [
+  {
+   "name": "Lonzo Ball, LAL PG"
+  },
+  {
+   "name": "Malcolm Brogdon, Mil SG, PG"
+  },
+  {
+   "name": "Brandon Ingram, LAL PF, SF"
+  },
+  {
+   "name": "Julius Randle, LAL PF"
+  },
+  {
+   "name": "John Collins, Atl PF, C"
+  },
+  {
+   "name": "Tim Hardaway Jr., NY SG, SF"
+  },
+  {
+   "name": "Bogdan Bogdanovic, Sac SG, SF"
+  },
+  {
+   "name": "Damian Lillard, Por PG"
+  },
+  {
+   "name": "Myles Turner, Ind PF, C"
+  },
+  {
+   "name": "Dario Saric, Phi SF, PF"
+  },
+  {
+   "name": "Buddy Hield, Sac SG"
+  },
+  {
+   "name": "Clint Capela, Hou C"
+  },
+  {
+   "name": "Zach LaVine*, Chi PG, SG O"
+  }
+ ],
+ "Alan Mundy": [
+  {
+   "name": "Darren Collison, Ind PG"
+  },
+  {
+   "name": "Klay Thompson, GS SG"
+  },
+  {
+   "name": "Taurean Prince, Atl SF"
+  },
+  {
+   "name": "Nikola Jokic, Den PF, C"
+  },
+  {
+   "name": "DeAndre Jordan, LAC C"
+  },
+  {
+   "name": "Donovan Mitchell, Utah SG"
+  },
+  {
+   "name": "Josh Jackson, Pho SF"
+  },
+  {
+   "name": "Dejounte Murray, SA SG, PG"
+  },
+  {
+   "name": "Jaylen Brown, Bos PF, SG, SF"
+  },
+  {
+   "name": "Andre Drummond, Det C"
+  },
+  {
+   "name": "Lauri Markkanen, Chi PF DTD"
+  },
+  {
+   "name": "Terry Rozier, Bos PG"
+  },
+  {
+   "name": "D'Angelo Russell, Bkn PG, SG DTD"
+  }
+ ],
+ "Allen Ng": [
+  {
+   "name": "John Wall, Wsh PG"
+  },
+  {
+   "name": "Devin Booker, Pho SG"
+  },
+  {
+   "name": "Khris Middleton, Mil SF, SG"
+  },
+  {
+   "name": "Blake Griffin, LAC PF"
+  },
+  {
+   "name": "Pau Gasol, SA PF, C"
+  },
+  {
+   "name": "Dwyane Wade, Cle SG"
+  },
+  {
+   "name": "Zach Randolph, Sac PF"
+  },
+  {
+   "name": "Kyrie Irving, Bos PG DTD"
+  },
+  {
+   "name": "Will Barton, Den SG, SF"
+  },
+  {
+   "name": "Robin Lopez, Chi C"
+  },
+  {
+   "name": "Markelle Fultz*, Phi PG O"
+  },
+  {
+   "name": "Trevor Booker, Bkn PF"
+  },
+  {
+   "name": "Jabari Parker*, Mil SF, PF O"
+  }
+ ],
+ "Nolan Nordlund": [
+  {
+   "name": "Jrue Holiday, Nor PG, SG"
+  },
+  {
+   "name": "Bradley Beal, Wsh SG"
+  },
+  {
+   "name": "Paul George, OKC SF"
+  },
+  {
+   "name": "Gorgui Dieng, Min C, PF DTD"
+  },
+  {
+   "name": "Marc Gasol, Mem C"
+  },
+  {
+   "name": "Elfrid Payton, Orl PG DTD"
+  },
+  {
+   "name": "Jayson Tatum, Bos SF"
+  },
+  {
+   "name": "Kris Dunn, Chi PG"
+  },
+  {
+   "name": "Greg Monroe*, Pho PF, C O"
+  },
+  {
+   "name": "Wilson Chandler, Den SF"
+  },
+  {
+   "name": "Justin Holiday, Chi SG, SF"
+  },
+  {
+   "name": "Seth Curry*, Dal SG O"
+  }
+ ],
+ "Eric Sewalson": [
+  {
+   "name": "Dennis Schroder, Atl PG"
+  },
+  {
+   "name": "Eric Gordon, Hou SG"
+  },
+  {
+   "name": "Jonathan Isaac, Orl SF DTD"
+  },
+  {
+   "name": "LeBron James, Cle SF, PF"
+  },
+  {
+   "name": "Marcin Gortat, Wsh C"
+  },
+  {
+   "name": "Jeff Teague, Min PG"
+  },
+  {
+   "name": "Dewayne Dedmon, Atl C"
+  },
+  {
+   "name": "Tobias Harris, Det PF, SF"
+  },
+  {
+   "name": "Rudy Gay, SA SF, PF"
+  },
+  {
+   "name": "Derrick Rose, Cle PG DTD"
+  },
+  {
+   "name": "Victor Oladipo, Ind SG"
+  },
+  {
+   "name": "Thaddeus Young, Ind SF, PF"
+  }
+ ],
+ "Adam Vickerman": [
+  {
+   "name": "Stephen Curry, GS PG DTD"
+  },
+  {
+   "name": "Jonathon Simmons, Orl SG"
+  },
+  {
+   "name": "Otto Porter Jr., Wsh SF"
+  },
+  {
+   "name": "Paul Millsap, Den PF"
+  },
+  {
+   "name": "Jusuf Nurkic, Por C"
+  },
+  {
+   "name": "Rodney Hood, Utah SG"
+  },
+  {
+   "name": "Kevin Love, Cle PF, C"
+  },
+  {
+   "name": "Kemba Walker, Cha PG"
+  },
+  {
+   "name": "Marcus Smart, Bos PG, SG"
+  },
+  {
+   "name": "Josh Richardson, Mia SG, SF"
+  },
+  {
+   "name": "Rondae Hollis-Jefferson, Bkn SF, PF"
+  },
+  {
+   "name": "Willie Cauley-Stein, Sac C"
+  },
+  {
+   "name": "Rudy Gobert*, Utah C O"
+  }
+ ],
+ "Jake Mlsna": [
+  {
+   "name": "CJ McCollum, Por PG, SG"
+  },
+  {
+   "name": ""
+  },
+  {
+   "name": "Robert Covington, Phi PF, SF"
+  },
+  {
+   "name": "Kristaps Porzingis, NY PF"
+  },
+  {
+   "name": "DeMarcus Cousins, Nor C"
+  },
+  {
+   "name": ""
+  },
+  {
+   "name": "Jae Crowder, Cle SF, PF"
+  },
+  {
+   "name": "DeMar DeRozan, Tor SG"
+  },
+  {
+   "name": "James Johnson, Mia SF, PF"
+  },
+  {
+   "name": "Goran Dragic, Mia PG"
+  },
+  {
+   "name": "Dion Waiters, Mia SG"
+  },
+  {
+   "name": "Jerian Grant, Chi PG"
+  },
+  {
+   "name": "Alex Len, Pho C"
+  },
+  {
+   "name": "Bojan Bogdanovic, Ind SG, SF"
+  },
+  {
+   "name": "Chris Paul*, Hou PG O"
+  },
+  {
+   "name": "Isaiah Thomas*, Cle PG O"
+  }
+ ],
+ "Chad Cox": [
+  {
+   "name": "Austin Rivers, LAC SG, PG"
+  },
+  {
+   "name": "Evan Fournier, Orl SG, SF"
+  },
+  {
+   "name": "TJ Warren, Pho SF"
+  },
+  {
+   "name": "Kyle Kuzma, LAL PF"
+  },
+  {
+   "name": "Enes Kanter, NY C"
+  },
+  {
+   "name": "Tyreke Evans, Mem SF, SG"
+  },
+  {
+   "name": "Anthony Davis, Nor PF"
+  },
+  {
+   "name": "Russell Westbrook, OKC PG"
+  },
+  {
+   "name": "Kyle Lowry, Tor PG"
+  },
+  {
+   "name": "Kentavious Caldwell-Pope, LAL SG"
+  },
+  {
+   "name": "Kelly Olynyk, Mia C, PF"
+  },
+  {
+   "name": "Trevor Ariza, Hou SF"
+  }
+ ],
+ "Brendan Dlugosch": [
+  {
+   "name": "Ricky Rubio, Utah PG"
+  },
+  {
+   "name": "Andrew Wiggins, Min SG"
+  },
+  {
+   "name": "Aaron Gordon, Orl PF, SF"
+  },
+  {
+   "name": "Ben Simmons, Phi PF, PG"
+  },
+  {
+   "name": "Jonas Valanciunas, Tor C"
+  },
+  {
+   "name": "De'Aaron Fox, Sac PG"
+  },
+  {
+   "name": "Skal Labissiere, Sac PF"
+  },
+  {
+   "name": "Justise Winslow, Mia SG, SF"
+  },
+  {
+   "name": "JJ Redick, Phi SG"
+  },
+  {
+   "name": "Steven Adams, OKC C DTD"
+  },
+  {
+   "name": "Kawhi Leonard*, SA SF O"
+  },
+  {
+   "name": "Tristan Thompson*, Cle PF, C O"
+  }
+ ]
+}
+
+export default rosters;
